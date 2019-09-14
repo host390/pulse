110726b873eeeea22f1c9fbc9202e822c2c935d8
Crak <alexander.fox98@mail.ru>
2019-09-14 20:13:28 +0300

    header, main, advantages

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const rename = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+// Static server
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+});
+
+gulp.task('gosass', function() {
+    return gulp.src("src/sass/**/*.+(sass|scss)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({
+            prefix: "",
+            suffix: ".min",
+          }))
+        .pipe(autoprefixer({
+            // browser: ['last 2 versions'],
+            cascade: false
+          }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(sass|scss)", gulp.parallel('gosass'));
+    gulp.watch("src/*.html").on("change", browserSync.reload);
+});
+
+gulp.task('default', gulp.parallel('server', 'gosass', 'watch'));
